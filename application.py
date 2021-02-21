c28b3fcfad7ab705f5e37120d326adc4abcdc3a5
Lucas <lzygarewicz@gmail.com>
2021-02-21 11:31:22 -0300

    Making progress

--- application.py
+++ application.py
@@ -1,6 +1,36 @@
-from flask import Flask, redirect, render_template, session
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+import random
+import cs50
+
 app = Flask(__name__)
+app.secret_key="dev"
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    username = 'admin'
+    password = str(random.randrange(100000000))
+    print(f"Pass: {password}")
+
+    if request.method == 'POST':
+        input_u = request.form.get("username")
+        input_p = request.form.get("password")
+
+        if input_u != username or input_p != password:
+            flash('Unable to login')
+            return render_template('login.html')
+
+        return redirect(url_for('control'))
+    else:
+        return render_template ('login.html')
+
+@app.route('/control', methods=['GET', 'POST'])
+def control():
+    if request.method == 'POST':
+        return render_template ('control.html')
+    else:
+        flash('Login needed')
+        return redirect(url_for('login'))
