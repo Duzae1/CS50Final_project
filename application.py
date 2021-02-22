9800e6e0671e2f44d0d2bbde2e831f02609696d4
Lucas <lzygarewicz@gmail.com>
2021-02-22 17:17:34 -0300

    Update

--- application.py
+++ application.py
@@ -6,9 +6,36 @@ app = Flask(__name__)
 app.secret_key="dev"
 db = SQL('sqlite:///app.db')
 
-@app.route('/')
+combos=[1, 2, 3, 4]
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        if request.form.get('name') == '':
+            flash('Please input your name')
+            return redirect(url_for('index'))
+        elif not int(request.form.get('combo')) in combos:
+            flash('Combo not available')
+            return redirect(url_for('index'))
+        
+        name = request.form.get('name')
+        email = request.form.get('email')
+        combo = int(request.form.get('combo'))
+
+        address = request.form.get('address')
+        table = request.form.get('table')
+
+        if request.form.get('delivery') == True:
+            db.execute('INSERT INTO orders (name, email, combo, address) VALUES (?, ?, ?, ?)', name, email, combo, address)
+        else:
+            db.execute('INSERT INTO orders (name, email, combo, table_n) VALUES (?, ?, ?, ?)', name, email, combo, address)
+
+        flash('Successful order!')
+        return redirect(url_for('index'))    
+    
+    else:
+        return render_template('index.html', combos=combos)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -33,8 +60,8 @@ def login():
 @app.route('/control', methods=['GET', 'POST'])
 def control():
     if request.method == 'POST':
-        
-        return render_template ('control.html')
+        orders = db.execute('SELECT * FROM orders')
+        return render_template ('control.html', orders=orders)
     else:
         flash('Login needed')
         return redirect(url_for('login'))
