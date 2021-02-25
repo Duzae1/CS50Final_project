881696fdc4ca21f2b08833374f41dc85f5b000ae
Lucas <lzygarewicz@gmail.com>
2021-02-25 12:32:51 -0300

    Now app sends mail

--- application.py
+++ application.py
@@ -1,11 +1,21 @@
-from flask import Flask, flash, redirect, render_template, request, session, url_for
 import random
+
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_mail import Mail, Message
 from cs50 import SQL
 
 # setup flask app and database
 app = Flask(__name__)
 app.secret_key="dev"
 db = SQL('sqlite:///app.db')
+
+app.config["MAIL_DEFAULT_SENDER"] = 'selfservebot@gmail.com'
+app.config["MAIL_PASSWORD"] = 'iooisrlvxbhhrarw'
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = 'selfservebot@gmail.com'
+mail = Mail(app)
 
 # list off the diferent combos
 combos=[1, 2, 3, 4]
@@ -26,17 +36,22 @@ def index():
         # retrieve form values
         name = request.form.get('name')
         email = request.form.get('email')
-        combo = int(request.form.get('combo'))
+        combo = request.form.get('combo')
         table = 'Table: ' + request.form.get('table')
         address = 'Address: ' + request.form.get('address')
-        
-
         radios = request.form.getlist('location')
+        
         if 'pick_up' in radios:
             db.execute('INSERT INTO orders (name, email, location, combos) VALUES (?, ?, ?, ?)', name, email, 'Picks it up', combo)
         elif 'on_place' in radios:
+            if request.form.get('table') == '':
+                flash("Please input your table")
+                return redirect(url_for('index'))
             db.execute('INSERT INTO orders (name, email, location, combos) VALUES (?, ?, ?, ?)', name, email, table, combo)
         elif 'delivery' in radios:
+            if request.form.get('address') == '':
+                flash("Please input your address")
+                return redirect(url_for('index'))
             db.execute('INSERT INTO orders (name, email, location, combos) VALUES (?, ?, ?, ?)', name, email, address, combo)
         else:
             flash('Invalid order')
@@ -53,21 +68,22 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # assign a username and create a different password each time someone tries to access the login form
-    # print the password to the command line
+    # assign user and password
     username = 'admin'
     password = '1234'
 
     if request.method == 'POST':
-        # check for correct valid and correct input
+        # check for valid and correct input
         input_u = request.form.get("username")
         input_p = request.form.get("password")
+
+        print(f'{input_u}, {input_p}')
 
         if input_u != username or input_p != password:
             flash('Unable to login')
             return render_template('login.html')
 
-        return redirect(url_for('control'))
+        return redirect(url_for('control'), code=308)
     else:
         # show login form
         return render_template ('login.html')
@@ -76,9 +92,39 @@ def login():
 @app.route('/control', methods=['GET', 'POST'])
 def control():
     if request.method == 'POST':
-        # return all orders
-        orders = db.execute('SELECT * FROM orders')
+        if request.form.get('save') == 'save':
+            order_id = int(request.form.get('counter'))
+            user_data = db.execute('SELECT name, email, location FROM orders WHERE id = ?', order_id)
+            name = user_data[0]['name']
+            email = user_data[0]['email']
+            where = user_data[0]['location']
+            if not email == None:
+                if 'Address' in where: 
+                    message = Message(f"Your order is on the way {name}!", recipients=[email])
+                    mail.send(message)
+                if 'Picks it up' in where:
+                    message = Message(f"{name}, you can pick up your order!", recipients=[email])
+                    mail.send(message)
+            db.execute('UPDATE orders SET done = 1 WHERE id = ?', order_id)
+
+        if request.form.get('delete') == 'delete':
+            order_id = int(request.form.get('counter'))
+            db.execute('DELETE FROM orders WHERE id = ?', order_id)
+
+        orders = db.execute('SELECT * FROM orders WHERE done = 0')
         return render_template ('control.html', orders=orders)
+          
+    else:
+            flash('Login needed')
+            return redirect(url_for('login'))
+       
+            
+
+@app.route('/log', methods=['GET', 'POST'])
+def log():
+    if request.method == 'POST':
+        orders = db.execute('SELECT * FROM orders')
+        return render_template ('log.html', orders=orders)
     else:
         flash('Login needed')
         return redirect(url_for('login'))
--- static/functions.js
+++ static/functions.js
@@ -4,16 +4,24 @@ function change_input(event) {
     {
         document.getElementById('address').style.display = 'none';
         document.getElementById('table').style.display = 'none';
+        document.getElementById('email').disabled = false
     }
     else if (radio.value == 'on_place')
     {
         document.getElementById('address').style.display = 'none';
         document.getElementById('table').style.display = 'block';
+        document.getElementById('email').value = ''
+        document.getElementById('email').disabled = true
     }
     else if (radio.value == 'delivery')
     {
         document.getElementById('address').style.display = 'block';
         document.getElementById('table').style.display = 'none';
+        document.getElementById('email').disabled = false
     }
     
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    $('[data-toggle="popover"]').popover()
+}, false);
